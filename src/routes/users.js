df1970ef752cf0c671de5e37c86873266a06a51b
maTORIx <matorixmatrix@gmail.com>
2017-08-17 15:10:19 +0900

    loginを修正した

--- src/routes/users.js
+++ src/routes/users.js
@@ -34,51 +34,20 @@ module.exports = function(app) {
     var password = req.body.password;
     var email = req.body.email;
 
-    // User.collection().where({email: email}).then((users) => {
-    //   if(users.length < 1) {
-    //     throw new Error("User not found");
-    //   }
-
-    //   let user = users[0];
-    //   let salt = user.data.salt;
-    //   var hash = crypto.createHash("sha512");
-    //   hash.update(salt);
-    //   hash.update(password);
-    //   var hashData = hash.digest("hex");
-
-    //   if(hashData !== user.data.password) {
-    //     throw new Error("password is not match");
-    //   }
-
-    //   let session = new UserSession({user_id: user.data.id});
-
-    //   return session.save();
-    // }).then((session) => {
-    //   res.cookie("session_id", session.data.id, {
-    //     path: "/",
-    //     httpOnly: true,
-    //     expires: new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)),
-    //     signed: true
-    //   });
-    //   res.redirect("/");
-    // }).catch((err) => {
-    //   res.render("login", {error: true});
-    // })
+    User.authenticate(email, password).then((user) => {
+      return UserSession.create(user);
+    }).then((session) => {
+      console.log("OK");
+      res.cookie("session_id", session.data.id, {
+        path: "/",
+        httpOnly: true,//http接続の時のみしか見ることができないCookie
+        expires: new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)),//期限
+        signed: true
+      });
+      res.redirect("/");
+    }).catch((err) => {
+      console.log(err);
+      res.render("login", {error: true});
+    })
   });
-
-  // app.get("/logout", function(req, res) {
-  //   res.cookie("userID", undefined);
-  //   res.redirect("/login")
-  // });
-
-  // app.get("/users/:id", function(req, res) {
-  //   let id = req.params.id;
-  //   app.locals.db.query(
-  //     "SELECT * FROM `articles` WHERE `user_id` = ? ",
-  //     [id],
-  //     function(error, result, render) {
-  //       let articles = result;
-  //       res.render("articles", {articles: articles})
-  //     });
-  // })
 }
