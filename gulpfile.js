3b902e4e8e131ca8591d574710627aeaa5fcebc8
maTORIx <matorixmatrix@gmail.com>
2017-08-21 15:23:48 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const express = require("gulp-express");
+const webpack = require("webpack-stream");
+
+gulp.task("server", function() {
+  express.run(['src/index.js']);
+
+  gulp.watch(["frontend/**/*.js"], ["webpack"]);
+  gulp.watch(["src/**/*.js"], [express.run]);
+})
+
+gulp.task("webpack", function() {
+  return gulp.src("frontend/index.js").pipe(webpack(require("./webpack.config.js"))).pipe(gulp.dest('public/'));
+})
